d5ead403245d25585e42742c54aa6968aded09aa
admin <admin>
2016-03-15 13:51:42 +0200

    template

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gitTempApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gitTempApp
+ */
+angular.module('gitTempApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gitTempApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gitTempApp
+ */
+angular.module('gitTempApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
